7c36be645a2088b50b72b50c9ddb306840688055
MrRain1 <b.degliuberti@outlook.com>
2022-04-07 15:59:44 +0200

    Successfully passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(strng,num) {
+    let concat="";
+    if(num<0){
+        return 'ERROR';
+    }
+    else{
+        for(let i=0;i<num;i++){
+            concat= concat + strng;
+        }
+    }
+    return concat;
 };
 
 // Do not edit below this line
