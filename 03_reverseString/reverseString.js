073463727415fca0bef28e49d276bf6d06d2e8b1
MrRain1 <b.degliuberti@outlook.com>
2022-04-07 16:30:07 +0200

    Ex3 passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(strng) {
+    //array vuoto da ritronare con la stringa in ingresso invertita
+    let array = [];
+    //trasformo la stringa in un array
+    let stringToArray = strng.split("");
 
+    //Ciclo per inserire come nuovo elemento una lettera della stringa in ordine inverso
+    for(let i=0; i<stringToArray.length; i++){
+        array.push(stringToArray[(stringToArray.length)-i-1]);
+    }
+
+    //unisco i termini dell'array in una stringa non separata da altri caratteri
+    return array.join("");
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
