483b07078fbc93c5445f87a1c7aae8f445fa7cac
MrRain1 <b.degliuberti@outlook.com>
2022-04-08 22:09:00 +0200

    Ex4 passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...args) {
+    //pointer to track the position in the input array
+    let pointerInputArray = 0;
 
+    //pointer to track the position in the array that contains the elements to eventually remove
+    let pointerArgs = 0;
+
+    //cycle to traverse the array containing the elements to remove
+    for(pointerArgs = 0; pointerArgs < args.length; pointerArgs++){
+
+        //cycle to traverse the array containing the inputValues
+        for(pointerInputArray = 0; pointerInputArray < inputArray.length; pointerInputArray++){
+            
+            //confronts the type of element we want to remove with the one in the input array
+            if(typeof(inputArray[pointerInputArray]) === typeof(args[pointerArgs])){
+                
+                //confronts if the element we want to remove is in the array
+                if(args[pointerArgs] === inputArray[pointerInputArray]){
+                    inputArray.splice(pointerInputArray, 1);
+                }
+            }
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
