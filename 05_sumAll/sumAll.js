a3fe8a1dc18fd0f2139a70dc4c8f4ace95bf9362
MrRain1 <b.degliuberti@outlook.com>
2022-04-09 18:35:18 +0200

    All test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,40 @@
-const sumAll = function() {
+const sumAll = function(startNumber, elementsToSum) {
+    //variabile per contenere la somma dei numeri
+    let sum = 0;
+    let temp = 0;
+    const error= "ERROR";
+
+    //check se tutti i parametri passati sono numeri
+    if((Number.isInteger(startNumber)) && (Number.isInteger(elementsToSum))){
+    
+        //Check for negative numbers
+        if(startNumber < 0 || elementsToSum < 0){
+            return error;
+        }
+
+        //Swap the arguments if the first in grater than the second
+        if(startNumber > elementsToSum){
+        temp=startNumber;
+        startNumber = elementsToSum;
+        elementsToSum = temp;
+        temp=0;
+        }
+
+        //Execute the sum iteration
+        for(let counter=startNumber; counter<=elementsToSum; counter++){
+        sum = sum + counter; 
+        }
+
+        return sum;
+    }
+    else{
+        return error;
+    }
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempFahr) {
+  let toCelsius = ((tempFahr - 32)*(5/9)).toFixed(1);
+   
+  return Number(toCelsius);
 };
 
-const ctof = function() {
+const ctof = function(tempCels) {
+  let toFahren = ((tempCels*9/5)+32).toFixed(1);
 
+  return Number(toFahren);
 };
 
 // Do not edit below this line
