94488dd29782abc560d31751d8858b0cfa631928
MrRain1 <b.degliuberti@outlook.com>
2022-07-05 10:15:37 +0200

    Finiti Esercizi

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let sum=0;
+  for(let i=0; i< a.length; i++){
+    sum+=a[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let mult = 1;
+  for(let i=0; i< a.length; i++){
+    mult = mult*a[i];
+  }
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a^b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a === 0 || a === 1){
+    return 1;
+  }
+  else if(a < 0){ return -1; }
+  else{
+    return a*factorial(a-1);
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+    const stringToLowercase = inputString.toLowerCase();
+    const strippedString = stringToLowercase.replace(/[.,/#!$%^&*;:{}=-_`~()]/g,"")
+                                            .replace(/\s+/g, "");
 
+    for(let i = 0; i < strippedString.length; i++){
+        if(strippedString[i] !== strippedString[(strippedString.length)-i-1]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
