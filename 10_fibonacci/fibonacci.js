0c8d565814869f3c89f0ad984ea6f2e4644cac99
MrRain1 <b.degliuberti@outlook.com>
2022-07-05 23:09:17 +0200

    Finiti esercizi

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    const exponent = parseInt(input);
+    const phi = (1+Math.sqrt(5))/2;
+    //Check input sign
+    if(exponent < 0) return "OOPS"; 
+    return parseInt((Math.pow(phi, exponent) - Math.pow(-phi, -exponent))/Math.sqrt(5));
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const sorted = people.sort((a,b) => {
+        const oldestAge = getAge(a.yearOfBirth, a.yearOfDeath);
+        const current = getAge(b.yearOfBirth, b.yearOfDeath);
+        return oldestAge > current ? -1 : 1;
+    });
+    return sorted[0];
 };
+
+const getAge = function(birth, death){
+    const now = new Date();
+    if(isNaN(death)){
+        death = now.getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
